d5aba55aaedb10778566938c82ec40274c632078
Ayush-Panwar <ayushpanwar717@gmail.com>
2022-08-27 17:28:50 +0530

    Deposit Tokens

--- blockchain-developer-bootcamp/test/Exchange.js
+++ blockchain-developer-bootcamp/test/Exchange.js
@@ -9,13 +9,22 @@ describe("Exchange", () => {
   let deployer;
   let feeAccount;
   let exchange;
+  let token1;
+  let user1;
   const feePercent = 10;
   beforeEach(async () => {
     accounts = await ethers.getSigners();
     deployer = accounts[0];
     feeAccount = accounts[1];
+    user1 = accounts[2];
     const Exchange = await ethers.getContractFactory("Exchange");
+    const Token = await ethers.getContractFactory("Token");
     exchange = await Exchange.deploy(feeAccount.address, feePercent);
+    token1 = await Token.deploy("DApp University", "DApp", "1000000");
+    let transaction = await token1
+      .connect(deployer)
+      .transfer(user1.address, tokens(100));
+    await transaction.wait();
   });
   describe("Deployment", () => {
     it("track the fee account ", async () => {
@@ -23,6 +32,50 @@ describe("Exchange", () => {
     });
     it("track the fee percent ", async () => {
       expect(await exchange.feePercent()).to.equal(feePercent);
+    });
+  });
+  describe("Depositing Tokens", () => {
+    let transaction, result;
+    let amount = tokens(10);
+
+    describe("Success", () => {
+      beforeEach(async () => {
+        transaction = await token1
+          .connect(user1)
+          .approve(exchange.address, amount);
+        result = await transaction.wait();
+
+        transaction = await exchange
+          .connect(user1)
+          .depositToken(token1.address, amount);
+        result = await transaction.wait();
+      });
+      it("tracks the token deposit", async () => {
+        expect(await token1.balanceOf(exchange.address)).to.equal(amount);
+        expect(await exchange.tokens(token1.address, user1.address)).to.equal(
+          amount
+        );
+        expect(
+          await exchange.balanceOf(token1.address, user1.address)
+        ).to.equal(amount);
+      });
+
+      it("emits a Deposit event", async () => {
+        const event = result.events[1];
+        expect(event.event).to.equal("Deposit");
+        const args = event.args;
+        expect(args.token).to.equal(token1.address);
+        expect(args.user).to.equal(user1.address);
+        expect(args.amount).to.equal(amount);
+        expect(args.balance).to.equal(amount);
+      });
+    });
+    describe("Failure", () => {
+      it("fails when no token were approved", async () => {
+        await expect(
+          exchange.connect(user1).depositToken(token1.address, amount)
+        ).to.be.reverted;
+      });
     });
   });
 });
