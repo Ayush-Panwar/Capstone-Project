d08e30c1f4ca9e6043bd98750f0fd9a8784916b4
Ayush-Panwar <ayushpanwar717@gmail.com>
2022-08-29 15:41:32 +0530

    Fill Orders

--- blockchain-developer-bootcamp/contracts/Exchange.sol
+++ blockchain-developer-bootcamp/contracts/Exchange.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.8.0;
 
-import "hardhat/console.sol";
+import "../node_modules/hardhat/console.sol";
 import "./Token.sol";
 
 contract Exchange {
@@ -10,6 +10,7 @@ contract Exchange {
  mapping(address=>mapping(address =>uint256)) public tokens;
  mapping(uint256  =>_Order) public orders;
  mapping(uint256=>bool) public orderCancelled;
+ mapping(uint256=>bool) public orderFilled;
 
  uint256 public orderCount;
 
@@ -45,7 +46,19 @@ address tokenGet,
 uint256 amountGet,
 address tokenGive,
 uint256 amountGive,
-uint256 timestamp);
+uint256 timestamp
+);
+
+event Trade(
+  uint256 id,
+  address user, 
+  address tokenGet,
+  uint256 amountGet,
+  address tokenGive,
+  uint256 amountGive,
+  address creator,
+  uint256 timestamp
+  );
 
 
   constructor(address _feeAccount,uint256 _feePercent) {
@@ -88,7 +101,7 @@ function makeOrder(address _tokenGet,uint256 _amountGet,address _tokenGive,uint2
  require(balanceOf(_tokenGive,msg.sender)>=_amountGive);
  
  
- orderCount =orderCount+1;
+ orderCount++;
    orders[orderCount] =_Order(
   1,
   msg.sender,
@@ -132,4 +145,45 @@ require(_order.id==_id);
   _order.amountGive,
   block.timestamp);}
 
+
+// ----------------------
+// EXECUTING ORDERS
+
+function fillOrder(uint256 _id) public{
+
+  require(_id>0 && _id<=orderCount,"Order does not count");
+
+  require(!orderFilled[_id]);
+
+  require(!orderCancelled[_id]);  
+
+  _Order storage _order =orders[_id];
+ 
+  _trade(
+  _order.id,
+  _order.user,
+  _order.tokenGet,
+  _order.amountGet,
+  _order.tokenGive,
+  _order.amountGive
+  );
+
+  orderFilled[_order.id]=true;
+
+}
+
+function _trade(uint256 _orderId,address _user, address _tokenGet,uint256 _amountGet,address _tokenGive, uint256 _amountGive) internal {
+uint256 _feeAmount =(_amountGet * feePercent)/100;
+
+tokens[_tokenGet][msg.sender]=tokens[_tokenGet][msg.sender]-(_amountGet +_feeAmount); // msg.sender here is user2
+tokens[_tokenGet][_user]=tokens[_tokenGet][_user]+_amountGet;
+
+tokens[_tokenGet][feeAccount]= tokens[_tokenGet][feeAccount]+_feeAmount;
+
+tokens[_tokenGive][_user]=tokens[_tokenGive][_user]- _amountGive;
+tokens[_tokenGive][msg.sender]=tokens[_tokenGive][msg.sender]+_amountGive;
+
+emit Trade(_orderId,msg.sender,_tokenGet,_amountGet,_tokenGive,_amountGive,_user,block.timestamp);
+
+}
 }
