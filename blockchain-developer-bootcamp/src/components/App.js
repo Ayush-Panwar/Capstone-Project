5c64c8f50e4c278f9f2b17142557b219dd34bd9e
Ayush-Panwar <ayushpanwar717@gmail.com>
2022-08-31 20:37:43 +0530

    Navbar

--- blockchain-developer-bootcamp/src/components/App.js
+++ blockchain-developer-bootcamp/src/components/App.js
@@ -9,6 +9,7 @@ import {
   loadTokens,
   loadExchange,
 } from "../store/interactions";
+import Navbar from "./Navbar";
 
 function App() {
   const dispatch = useDispatch();
@@ -19,8 +20,15 @@ function App() {
     //Fetch current network's chainId (e.g. hardhat:31337,kovan:42)
     const chainId = await loadNetwork(provider, dispatch);
 
-    //Fetch current account & balance from Metamask
-    const account = await loadAccount(provider, dispatch);
+    //Reload page when network changes
+    window.ethereum.on("chainChanged", () => {
+      window.location.reload();
+    });
+
+    //Fetch current account & balance from Metamask when changed
+    window.ethereum.on("accountsChanged", async () => {
+      await loadAccount(provider, dispatch(provider, dispatch));
+    });
 
     //Loadt token smart contracts
     const DApp = config[chainId].DApp;
@@ -41,7 +49,7 @@ function App() {
   });
   return (
     <div>
-      {/* Navbar */}
+      {<Navbar />}
 
       <main className="exchange grid">
         <section className="exchange__section--left grid">
